5b05c8e7ed70cbea36bc57ec437ce456bbd7a14f
shinichin <2252215674@qq.com>
2020-10-14 17:43:12 +0800

    1014

---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="info-type">
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="loginForm.username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="loginForm.password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Form } from 'vant'
+Vue.use(Form)
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      responseResult: []
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$axios
+        .post('/login', {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
+        .then(successResponse => {
+          if (successResponse.data.code === 200) {
+            this.$router.replace({path: '/index'})
+          }
+        })
+        .catch(failResponse => {
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.info-type{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+import Van from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Van)
 Vue.use(Router)
 
 export default new Router({
   routes: [
+
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
     }
   ]
 })
